236dede807f74bd6a1fdce5c54bf4c3f255acac4
Nelson Jr <juniordell123@gmail.com>
2020-07-28 22:18:57 -0300

    Add the structure

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import Header from "./components/header";
+import TodoItem from './components/todoItem'
 
 export default function App() {
+  const [ todos, setTodos ] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app' , key: '2' },
+    { text: 'play on the switch' , key: '3' },
+  ])
+
+  const pressHandler = key => {
+    setTodos(prev => {
+      return prev.filter(todo => todo.key != key)
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+
+        </View>
+
+      </View>
+
     </View>
   );
 }
@@ -15,7 +41,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  content: {
+    padding: 40
+  },
+
+  list: {
+    marginTop: 20
+  }
+
 });
